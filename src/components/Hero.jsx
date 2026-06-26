7497f12b53ea7d66b00bd0b84b2491afcb930858
raynegoldman <raynegoldman@gmail.com>
2026-06-27 04:28:26 +0530

    style: use user-uploaded HD image for Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import coverImg from "@/assets/images/new_content/Untitled folder 4/Suits/Sunil Mehra  0553.jpg";
+import coverImg from "@/assets/images/IMG_7216_HD.jpg";
 
 export default function Hero() {
     const brandName = "SUNIL MEHRA";
