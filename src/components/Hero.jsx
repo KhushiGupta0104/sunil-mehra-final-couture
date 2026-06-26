a90be970802bd8dd44791433e82dcb0a9e11c99c
raynegoldman <raynegoldman@gmail.com>
2026-06-27 04:38:35 +0530

    style: use 24MB raw HD image for Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import coverImg from "@/assets/images/IMG_7216_HD.jpg";
+import coverImg from "@/assets/images/hero_pic_123.jpg";
 
 export default function Hero() {
     const brandName = "SUNIL MEHRA";
